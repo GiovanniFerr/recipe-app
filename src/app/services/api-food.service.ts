74266340260f5d60c53dde4559bfd73f155c0e4e
GiovanniFerr <giovanni.ferrandino000@gmail.com>
2026-04-10 16:19:20 +0200

    implementation httpClient

---
+++ src/app/services/api-food.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiFoodService {
+  constructor(private http: HttpClient) {}
+
+  getRecipes() {
+    return this.http.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  }
+}
