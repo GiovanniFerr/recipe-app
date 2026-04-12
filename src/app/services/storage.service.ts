07e8a81c5d9db43f798a249b7aaec72e919ffa6b
GiovanniFerr <giovanni.ferrandino000@gmail.com>
2026-04-12 11:27:50 +0200

    service for local storage

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StorageService {}
+export class StorageService {
+  private platformId = inject(PLATFORM_ID);
+
+  get<T>(key: string): T | null {
+    if (isPlatformBrowser(this.platformId)) {
+      const data = localStorage.getItem(key);
+      return data ? JSON.parse(data) : null;
+    }
+    return null;
+  }
+
+  set(key: string, value: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }
+
+  remove(key: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(key);
+    }
+  }
+}
